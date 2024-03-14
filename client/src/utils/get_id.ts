8c5198f0ba5fbe357554c802436420c6073e21dc
Jay Patel <jay64441860@gmail.com>
2024-03-14 10:25:27 +0530

    removed client

--- client/src/utils/get_id.ts
+++
@@ -1,6 +0,0 @@
-export const getId = (id: string | null | undefined): string => {
-    if (id === null || id === undefined) {
-        return '';
-    }
-    return id.toLowerCase().replace(/[^a-z0-9]+/g, '-');
-};
--- client/src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-import { getId } from "./get_id";
-import { getStaticProps } from "./get_static_props";
-import { handleScroll } from "./scroll_to";
-
-export {
-    getId,
-    getStaticProps,
-    handleScroll,
-}
